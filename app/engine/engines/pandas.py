599ccc464f3a44c8ccab00a1c9163f7c465ba7d5
HassenMahdi <hassenmahdi77@gmail.com>
2020-10-02 10:36:28 +0200

    Bad Lines ignore errors

--- app/engine/engines/pandas.py
+++ app/engine/engines/pandas.py
@@ -8,7 +8,7 @@ class PandasEngine(DataFrameEngine):
     engine = pd
 
     def read_csv(self, filepath, *args, **kwargs):
-        self.frame = self.engine.read_csv(filepath, engine="c", dtype=str, skipinitialspace=True, na_filter=False,delimiter=";")
+        self.frame = self.engine.read_csv(filepath, error_bad_lines=False,engine="c", dtype=str, skipinitialspace=True, na_filter=False,delimiter=";")
         return self
 
     def __repr__(self):
