82122e5879ba304ca30cd40296ca9171f366d39c
HassenMahdi <hassenmahdi77@gmail.com>
2020-10-12 21:05:45 +0200

    Path Auto Create

---
+++ app/main/util/storage.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+from flask import current_app as app
+
+export_folder = os.path.join(app.config['UPLOAD_FOLDER'], 'exports')
+
+
+def get_path(folder, filename, as_folder=False, create=False):
+    """Creates the path for a file under UPLOAD FOLDER"""
+    folder_path = Path(folder)
+    if create:
+        folder_path.mkdir(parents=True, exist_ok=True)
+    if as_folder:
+        return str(folder_path)
+    else:
+        return str(folder_path.joinpath(filename))
+
+
+def get_export_path(filename):
+    return get_path(export_folder, filename)
