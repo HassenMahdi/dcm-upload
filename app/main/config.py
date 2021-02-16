8df8fb7e4faf0f978f6d732d87c65fa798caf0df
Wajdovic <wajdibenabderrahim@gmail.com>
2021-02-16 14:10:58 +0100

    Update config.py

--- app/main/config.py
+++ app/main/config.py
@@ -11,14 +11,10 @@ class Config:
     DEBUG = True
     # UPLOAD_FOLDER = "C:/Users/Wajdi"
     UPLOAD_FOLDER = "/scor-data"
-    # MONGO_URI = "mongodb://root:Bxia!2020DaaTa1920CAvlmd@a4ec5441fc63a4fefbc97353d13465d2-1236515762.eu-west-3.elb.amazonaws.com:27017/dcm?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false&retryWrites=false"
     MONGO_URI = os.getenv("MONGO_URI")
     AWS_KEY = os.getenv("AWS_KEY")
     AWS_SECRET = os.getenv("AWS_SECRET")
     AWS_REGION = os.getenv("AWS_REGION")
-    # AWS_KEY = "AKIAQHONPL2UAI43JG5S"
-    # AWS_SECRET = "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"
-    # AWS_REGION = "eu-west-3"
 
 class DevelopmentConfig(Config):
     # uncomment the line below to use postgres
